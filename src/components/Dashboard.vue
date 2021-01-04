e4aec179093f3c836cd73d415265c13d1b88f439
Matthieu Mourisson <mareek@gmail.com>
2021-01-05 00:33:16 +0100

    Affichage du nombre de décès hebdomadaires

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <Trend smooth auto-draw :data="casHebdomadaire" />
-    <p>{{ rawData }}</p>
+    <h1>Décès hebdomadaires</h1>
+    <Trend smooth auto-draw :data="decesHebdomadaire" />
+    <hr/>
   </div>
 </template>
 
 <script>
 import Trend from "vuetrend";
 import staticData from "../data/code-FRA.json";
+import _ from "lodash";
+
 const etalabUrl = "https://dashboard.covid19.data.gouv.fr/data/code-FRA.json";
 
 export default {
@@ -19,21 +22,25 @@ export default {
     };
   },
   computed: {
-    casHebdomadaire() {
-      return [8, 5, 7, 3, 12];
+    sortedData() {
+      return _.orderBy(this.rawData, ["date"]);
     },
-  },
-  async mounted() {
-    try {
-      const response = await fetch(etalabUrl);
-      this.rawData = await response.json();
-    } catch {
-      this.rawData = staticData;
-    }
-  },
-};
-
-/* raw data
+    deces() {
+      return this.getDailyValues("deces");
+    },
+    casConfirmes() {
+      return this.getDailyValues("casConfirmes");
+    },
+    hospitalises() {
+      return this.getDailyValues("hospitalises");
+    },
+    decesQuotidien() {
+      return this.getDailyDelta(this.deces);
+    },
+    decesHebdomadaire() {
+      return this.getWeeklyData(this.decesQuotidien);
+    },
+    /* raw data
 {
   "casConfirmes": 10995,
   "deces": 372,
@@ -51,6 +58,44 @@ export default {
   "tauxOccupationRea": 19.8
 }
 */
+  },
+  async mounted() {
+    try {
+      const response = await fetch(etalabUrl);
+      this.rawData = await response.json();
+    } catch {
+      this.rawData = staticData;
+    }
+  },
+  methods: {
+    getDailyValues(propertyName) {
+      if (!this.sortedData.length) {
+        return [];
+      }
+
+      const result = [this.sortedData[0][propertyName] || 0];
+      for (let i = 1; i < this.sortedData.length; i++) {
+        const dailyValue = this.sortedData[i][propertyName];
+        result[i] = dailyValue || result[i - 1];
+      }
+      return result;
+    },
+    getDailyDelta(dailyValues) {
+      const result = [];
+      for (let i = 0; i < dailyValues.length; i++) {
+        result[i] = dailyValues[i] - (dailyValues[i - 1] || 0);
+      }
+      return result;
+    },
+    getWeeklyData(dailyValues) {
+      const result = [];
+      for (let i = 7; i < dailyValues.length; i++) {
+        result.push(_.sum(dailyValues.slice(i - 7, i) || 0));
+      }
+      return result;
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
