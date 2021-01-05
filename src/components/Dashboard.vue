068044dfbf4b5d448113fff6533ff984a1b99093
Matthieu Mourisson <mareek@gmail.com>
2021-01-05 22:37:28 +0100

    Ajout grqphique circulation + refacto

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
-    <h1>Décès hebdomadaires</h1>
-    <Trend smooth auto-draw :data="decesHebdomadaire" />
-    <hr/>
+    <Graphique title="Décès" :values="decesHebdomadaire" />
+    <Graphique title="Circulation" :values="circulationHebdomadaire" />
   </div>
 </template>
 
 <script>
-import Trend from "vuetrend";
+import Graphique from "./Graphique.vue";
 import staticData from "../data/code-FRA.json";
 import _ from "lodash";
 
@@ -15,7 +14,7 @@ const etalabUrl = "https://dashboard.covid19.data.gouv.fr/data/code-FRA.json";
 
 export default {
   name: "Dashboard",
-  components: { Trend },
+  components: { Graphique },
   data() {
     return {
       rawData: [],
@@ -28,36 +27,50 @@ export default {
     deces() {
       return this.getDailyValues("deces");
     },
-    casConfirmes() {
-      return this.getDailyValues("casConfirmes");
-    },
-    hospitalises() {
-      return this.getDailyValues("hospitalises");
-    },
     decesQuotidien() {
       return this.getDailyDelta(this.deces);
     },
     decesHebdomadaire() {
       return this.getWeeklyData(this.decesQuotidien);
     },
-    /* raw data
-{
-  "casConfirmes": 10995,
-  "deces": 372,
-  "reanimation": 1122,
-  "hospitalises": 4461,
-  "gueris": 1300,
-  "date": "2020-03-19",
-  "code": "FRA",
-  "nom": "France",
-  "testsRealises": 2098,
-  "testsPositifs": 528,
-  "nouvellesHospitalisations": 2229,
-  "nouvellesReanimations": 438,
-  "tauxIncidence": 5.57082271463596,
-  "tauxOccupationRea": 19.8
-}
-*/
+    casConfirmes() {
+      return this.getDailyValues("casConfirmes");
+    },
+    casQuotidien() {
+      return this.getDailyDelta(this.casConfirmes);
+    },
+    casHebdomadaire() {
+      return this.getWeeklyData(this.casQuotidien);
+    },
+    hospitalises() {
+      return this.getDailyValues("hospitalises");
+    },
+    reanimation() {
+      return this.getDailyValues("reanimation");
+    },
+    testsRealises() {
+      return this.getDailyValues("testsRealises", () => 0);
+    },
+    testsPositifs() {
+      return this.getDailyValues("testsPositifs", () => 0);
+    },
+    positivite() {
+      return _.zipWith(
+        this.testsRealises,
+        this.testsPositifs,
+        (r, p) => p / r || 0
+      );
+    },
+    positiviteHebdomadaire() {
+      return this.getWeeklyData(this.positivite);
+    },
+    circulationHebdomadaire() {
+      return _.zipWith(
+        this.casHebdomadaire,
+        this.positiviteHebdomadaire,
+        (c, p) => c * p
+      );
+    },
   },
   async mounted() {
     try {
@@ -68,15 +81,16 @@ export default {
     }
   },
   methods: {
-    getDailyValues(propertyName) {
+    getDailyValues(propertyName, getDefaultValue) {
       if (!this.sortedData.length) {
         return [];
       }
 
       const result = [this.sortedData[0][propertyName] || 0];
+      getDefaultValue = getDefaultValue || ((i) => result[i - 1]);
       for (let i = 1; i < this.sortedData.length; i++) {
         const dailyValue = this.sortedData[i][propertyName];
-        result[i] = dailyValue || result[i - 1];
+        result[i] = dailyValue || getDefaultValue(i);
       }
       return result;
     },
@@ -89,15 +103,12 @@ export default {
     },
     getWeeklyData(dailyValues) {
       const result = [];
-      for (let i = 7; i < dailyValues.length; i++) {
-        result.push(_.sum(dailyValues.slice(i - 7, i) || 0));
+      for (let i = 0; i < dailyValues.length; i++) {
+        const lowerBound = Math.max(0, i - 7);
+        result.push(_.sum(dailyValues.slice(lowerBound, i) || 0));
       }
       return result;
     },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
---
+++ src/components/Graphique.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <Trend smooth auto-draw :data="values" />
+    <hr />
+  </div>
+</template>
+<script>
+import Trend from "vuetrend";
+
+export default {
+  name: "Graphique",
+  components: { Trend },
+  props: {
+    title: String,
+    values: { type: Array, default: () => [] },
+  },
+};
+</script>
